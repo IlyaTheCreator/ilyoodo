88a840c7637c501d6a42d420e27fa8225d96e107
ilya_the_creator <respondatanytime@gmail.com>
2021-05-11 14:41:43 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <user />
+  </div>
+</template>
+
+<script>
+import User from "./components/User";
+
+export default {
+  name: "App",
+  components: {
+    User,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  font-family: sans-serif;
+}
+
+#app {
+  height: 100vh;
+  width: 100%;
+  display: grid;
+  place-content: center;
+}
+</style>
---
+++ src/components/User.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="user">
+    <div class="wrapper" v-if="isFemale">
+      <h1>{{ firstName }} {{ lastName }}</h1>
+      <img :src="picture" />
+    </div>
+    <div v-else class="spinner">
+      <a-spin>
+        <a-icon
+          style="position: relative; left: -40px; font-size: 100px"
+          size="large"
+          slot="indicator"
+          type="loading"
+          spin
+        />
+      </a-spin>
+    </div>
+    <button @click="update">update</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      picture: "",
+      phone: "",
+      isFemale: false,
+    };
+  },
+  methods: {
+    update: function () {
+      this.fetchUser();
+    },
+    fetchUser() {
+      fetch("https://randomuser.me/api/")
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.results[0].gender === "female") {
+            this.isFemale = true;
+            this.firstName = data.results[0].name.first;
+            this.lastName = data.results[0].name.last;
+            this.picture = data.results[0].picture.large;
+          } else {
+            this.isFemale = false;
+            setTimeout(() => {
+              this.fetchUser();
+            }, 500);
+          }
+        })
+        .catch((error) => console.warn(error));
+    },
+  },
+  created() {
+    this.fetchUser();
+  },
+};
+</script>
+
+<style>
+.spinner {
+  display: grid;
+  place-content: center;
+  height: calc(100% - 87.75px);
+}
+
+.user {
+  display: flex;
+  flex-direction: column;
+  width: 350px;
+  height: 400px;
+  box-shadow: 1px 3px 46px 18px rgba(34, 60, 80, 0.2);
+  border-radius: 20px;
+  padding-top: 15px;
+  margin: 20px auto;
+}
+
+.user h1 {
+  text-align: center;
+  width: 350px;
+}
+
+.user .wrapper {
+  width: 350px;
+  align-self: center;
+  display: grid;
+  place-content: center;
+}
+
+.user img {
+  width: 200px;
+  height: 200px;
+  border-radius: 50%;
+  margin: 20px auto;
+}
+
+.user .getero {
+  height: 277px;
+}
+
+.user button {
+  width: 180px;
+  align-self: center;
+  padding: 15px 30px;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: 0;
+  border-radius: 15px;
+  cursor: pointer;
+  background: #fe7171;
+  color: #fff;
+  transition: all 0.2s ease;
+}
+
+.user button:hover {
+  box-shadow: 0 2px 10px rgba(0, 0, 0, 0.2);
+  background: #fe4a49;
+}
+</style>
